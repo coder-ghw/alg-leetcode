588bc96b0f6ba5cab6db914ad2b953bfecc31289
huwei.guo <huwei.guo@deptrum.com>
2023-03-31 10:15:00 +0800

    add bfs some code

---
+++ src/alg_bfs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+/**描述:
+ *
+ *
+ *
+ */
+int alg_bfs(const int* data, int w, int h)
+{
+  int rc = 0;
+  std::vector<int> node;
+  std::vector<int> visited;
+
+
+  return rc;
+}
+
+int main()
+{
+  int rc = 0;
+  rc = alg_bfs();
+
+  return 0;
+}
+
